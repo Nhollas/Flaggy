00760c10e250bb577f533a40ed9207d5418e3f41
Nick Hollas <nicholashollas@yahoo.co.uk>
2024-05-26 18:55:58 +0100

    Remove unused section tag.

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -1,9 +1,5 @@
 import { FlagContextBuilder } from "@/app/features/flags"
 
 export default async function Home() {
-  return (
-    <section className="w-full space-y-8">
-      <FlagContextBuilder />
-    </section>
-  )
+  return <FlagContextBuilder />
 }
